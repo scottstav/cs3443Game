7aa8fa56bce043e3c56c1b6fc50a0e378d4eaaa9
Paco Huerta <paco.huerta1218@gmail.com>
2015-11-16 22:02:06 -0600

    massive changes. I'll explain code tomorrow

---
+++ src/cs3443game/PowerUp.java
@@ -0,0 +1,49 @@
+package cs3443game;
+
+import java.awt.geom.Rectangle2D;
+
+public class PowerUp implements Collidable{
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRGB(int x, int y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Rectangle2D getBounds() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void collision() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
