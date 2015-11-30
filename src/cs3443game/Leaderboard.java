3b204316cee3725678f6b421299fa4751f10ef95
Kurt King <kurtaking@gmail.com>
2015-11-29 19:46:04 -0600

    Updates need to be merged. Attempt 2

---
+++ src/cs3443game/Leaderboard.java
@@ -0,0 +1,10 @@
+package cs3443game;
+
+import java.io.Serializable;
+
+public class Leaderboard implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+
+}
---
+++ src/cs3443game/Player.java
@@ -0,0 +1,31 @@
+package cs3443game;
+
+import java.util.ArrayList;
+
+public class Player implements Comparable<Player>
+{
+	ArrayList<Player> players = new ArrayList<Player>();
+	private String username;
+	private int score;
+	
+	/**
+	 * Constructor for setting up a user
+	 */
+	public Player(String name)
+	{
+		username = name;
+		score = 0;
+	}
+
+	@Override
+	public int compareTo(Player o)
+	{
+		return 0;
+	}
+	
+	public String toString()
+	{
+		return username + score;
+	}
+	
+}
